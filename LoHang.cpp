49e6f2867f9de34192eb618dacd7660a18aa0ab8
Minh <78477600+vnminh@users.noreply.github.com>
2023-10-31 23:22:13 +0700

    Update LoHang.cpp

--- LoHang.cpp
+++ LoHang.cpp
@@ -1,13 +1,13 @@
 #include "LoHang.h"
 #include <iostream>
 using namespace std;
-LoHang::LoHang(const String& id, const Date& nn, const Date& nsx, const Date& hsd, const int& sl)
-	:ID(id), NN(nn), NSX(nsx), HSD(hsd), SL(sl)
+LoHang::LoHang(const String& id, const Date& nn, const Date& nsx, const Date& hsd,const int& price, const int& sl)
+	:ID(id), NN(nn), NSX(nsx), HSD(hsd),Price(price), SL(sl)
 {}
 LoHang::~LoHang()
 {}
 ostream& operator<<(ostream& out, const LoHang & lo)
 {
-	out << lo.ID << ' ' << lo.NN << ' ' << lo.NSX << ' ' << lo.HSD << ' ' << lo.SL;
+	out << lo.ID << ' ' << lo.NN << ' ' << lo.NSX << ' ' << lo.HSD << ' ' << lo.Price << ' ' << lo.SL;
 	return out;
 }
